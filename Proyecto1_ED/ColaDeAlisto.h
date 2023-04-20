825d3c10d926f7c2f8b69f96e788464f55800221
urenakiller10 <125626887+urenakiller10@users.noreply.github.com>
2023-04-19 23:45:49 -0600

    Se avanza en "fábricas de productos.h" y en "Cola de alisto"
    Con esto, al menos con la decisión que tome el balanceador (revisa cantidad del producto y con eso la toma) lo manda a la cola de alisto o a la fabrica, que por ahora lo que hace es imprimir en cual de las 3 fabricas se fabrica y el tiempo que dura haciendose dicho producto. En el caso de la cola de alisto lo que hace es agregarlo a esta cola si hay de la cantidad, para que después entre en juego la bodegaAlisto_alistadores...pero eso es después, por ahora se necesita unificar las partes para ir "al día".

--- Proyecto1_ED/ColaDeAlisto.h
+++ Proyecto1_ED/ColaDeAlisto.h
@@ -2,4 +2,30 @@
 #ifndef COLADEALISTO_H
 #define COLADEALISTO_H
 
+#include <queue>
+
+class ColaAlisto {
+public:
+
+    //Nos va a funcionar para agregar a la cola un elemento
+
+    void agregarElemento(int elemento) {
+        cola.push(elemento);
+    }
+
+    //Ojo con esta, nos va a servir para recuperar y procesar(ir pasando a la bodega de alisto) los elementos en el orden que se fueron agregando a la cola
+    int obtenerProximoElemento() {
+        int elemento = cola.front();
+        cola.pop();
+        return elemento;
+    }
+
+    bool estaVacia() {
+        return cola.empty();
+    }
+
+private:
+    std::queue<int> cola;
+};
+
 #endif // COLADEALISTO_H
