b0e70115586140e450385dd89278ad53d3ac4d46
urenakiller10 <125626887+urenakiller10@users.noreply.github.com>
2023-04-24 23:00:26 -0600

    Se implementa la cola de facturación, en donde se crean los txt de cada pedido de la colad de alistados que recibe

--- Proyecto1_ED/ColaFacturacion.h
+++ Proyecto1_ED/ColaFacturacion.h
@@ -2,4 +2,68 @@
 #ifndef COLAFACTURACION_H
 #define COLAFACTURACION_H
 
+#include <fstream>
+#include <queue>
+#include <string>
+#include <QDir>
+
+class colaFacturacion {
+public:
+    colaFacturacion() {
+        // Inicializar la cola con tres pedidos
+        pedidos.push("Pedido1");
+        pedidos.push("Pedido2");
+        pedidos.push("Pedido3");
+    }
+
+    void generarFacturas() {
+        QDir::setCurrent(QCoreApplication::applicationDirPath());
+        int numFactura = 1;
+        while (!pedidos.empty()) {
+            // Obtener el siguiente pedido de la cola
+            std::string pedido = pedidos.front();
+            pedidos.pop();
+
+            // Construir el nombre del archivo de texto
+            std::string nombreArchivo = "Factura" + std::to_string(numFactura) + ".txt";
+
+            // Crear el archivo de texto y escribir el contenido
+            std::ofstream archivo(nombreArchivo);
+            archivo << "Pedido: " << numFactura << std::endl;
+            archivo << "Cliente: " << std::endl;
+            archivo << "----------------------------------------------------------------" << std::endl;
+            archivo << "En cola: " << std::endl;
+            archivo << "Balanceador: " << std::endl;
+            archivo << "A fabrica: " << std::endl;
+            archivo << "A alisto: " << std::endl;
+            archivo << "A empaque: " << std::endl;
+            archivo << "Finalizado: " << std::endl;
+            archivo << "----------------------------------------------------------------" << std::endl;
+            archivo << "FABRICA" << std::endl;
+            archivo << "Articulo: " << std::endl;
+            archivo << "Cantidad: " << std::endl;
+            archivo << "Inicio: " << std::endl;
+            archivo << "Final: " << std::endl;
+            archivo << "--------------------------------------------------------------" << std::endl;
+            archivo << "ALISTO" << std::endl;
+            archivo << "Alistador: 4" << std::endl;
+            archivo << "Codigo articulo: / Ubicación: / Tiempo" << std::endl;
+
+
+            // Incrementar el número de factura para el próximo archivo
+            numFactura++;
+        }
+    }
+
+private:
+    std::queue<std::string> pedidos;
+};
+
+
+
+
+
+
+
+
 #endif // COLAFACTURACION_H
--- Proyecto1_ED/main.cpp
+++ Proyecto1_ED/main.cpp
@@ -3,39 +3,50 @@
 #include "SubirAMemoriaArticulos.h"
 #include "HiloPedidos.h"
 #include "BalanceadorDecisiones.h"
-
+#include "ColaFacturacion.h"
 
 int main(int argc, char *argv[])
 {
-    //Se muestra la ventana con la interfaz gráfica
-    QApplication a(argc, argv);
-    Proyecto_ED w;
-    w.show();
+    QApplication app(argc, argv);
 
-    //Iniciar el hilo de lectura de pedidos
-    FileRead hiloPedidos;
-    hiloPedidos.start();
+//    //Se muestra la ventana con la interfaz gráfica
+//    QApplication a(argc, argv);
+//    Proyecto_ED w;
+//    w.show();
 
-    //Cargar CLientes Memoria.h
-    list<Cliente> listaClientes;
-    LeerClientes(listaClientes);
-    cout<<"hola"<<endl;
+//    //Iniciar el hilo de lectura de pedidos
+//    FileRead hiloPedidos;
+//    hiloPedidos.start();
 
-    //Subir a Memoria Articulos.h
-    list<Articulo> articulos;
-    subirArticulos(articulos);
+//    //Cargar CLientes Memoria.h
+//    list<Cliente> listaClientes;
+//    LeerClientes(listaClientes);
+//    cout<<"hola"<<endl;
 
-    // Imprimir la lista de artículos del txt
-    for (const auto& articulo : articulos) {
-        cout << articulo.codigo << '\t' << articulo.cantidad << '\t'
-             << articulo.tiempo << '\t' << articulo.categoria << '\t'
-             << articulo.ubicacion << endl;
-    }
+//    //Subir a Memoria Articulos.h
+//    list<Articulo> articulos;
+//    subirArticulos(articulos);
+
+//    // Imprimir la lista de artículos del txt
+//    for (const auto& articulo : articulos) {
+//        cout << articulo.codigo << '\t' << articulo.cantidad << '\t'
+//             << articulo.tiempo << '\t' << articulo.categoria << '\t'
+//             << articulo.ubicacion << endl;
+//    }
+
+    // Se llama al encargado de facturar la cola de alistados
+
+
+    colaFacturacion cola;
+    cola.generarFacturas();
+    return 0;
+
+
 
     //Llamado del BalanceadorDecisiones
 
     //BalanceadorDecisiones();
 
-    return a.exec();
+    //return a.exec();
 
 }
