fbc2e6cd64e3bf6328fc91738ab8514cac7e3c5b
urenakiller10 <125626887+urenakiller10@users.noreply.github.com>
2023-04-26 08:46:06 -0600

    Cola alistados

--- Proyecto1_ED/ColaAlistados.h
+++ Proyecto1_ED/ColaAlistados.h
@@ -2,4 +2,25 @@
 #ifndef COLAALISTADOS_H
 #define COLAALISTADOS_H
 
+#include <queue>
+#include <string>
+
+class colaAlistados {
+public:
+    colaAlistados();
+    ~colaAlistados();
+
+    void agregarAlista(std::string elemento);
+
+private:
+    std::queue<std::string> listaAlistados;
+};
+
+colaAlistados::colaAlistados() {}
+colaAlistados::~colaAlistados() {}
+
+void colaAlistados::agregarAlista(std::string elemento) {
+    listaAlistados.push(elemento);
+}
+
 #endif // COLAALISTADOS_H
