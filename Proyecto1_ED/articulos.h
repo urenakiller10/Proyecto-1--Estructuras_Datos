935c48ce80c752d07b611eb3eaa594b9f019c1d3
Charlie00CR <113566517+Charlie00CR@users.noreply.github.com>
2023-04-27 00:14:08 -0600

    lista doble

--- Proyecto1_ED/articulos.h
+++ Proyecto1_ED/articulos.h
@@ -1,11 +1,11 @@
-#include "includes.h"#
+#include "includes.h"
 #include "auxiliaries.h"
 #ifndef ARTICULOS_H
 #define ARTICULOS_H
 
 //Esta wea se va a encargar de mantener la bodega
 
-struct articulo{
+struct Articulo{
     //Data section
     string codigo;
     int existencias;
@@ -14,16 +14,20 @@ struct articulo{
     string ubicacion;
 
     //Pointer section
-    articulo* anterior;
-    articulo* siguiente;
+    Articulo* anterior;
+    Articulo* siguiente;
 
     //Builder
-    articulo(string _codigo, int _cantidadAlmacen, int _segundosFabricacion, string _categoria, string _ubicacion){
+    Articulo(string _codigo, int _cantidadAlmacen, int _segundosFabricacion, string _categoria, string _ubicacion){
         codigo = _codigo;
         existencias = _cantidadAlmacen;
         segundosFabricacion = _segundosFabricacion;
         categoria = _categoria;
         ubicacion = _ubicacion;
+    }
+
+    void imprimir(){
+        cout << "Code:" <<codigo <<" Cant:" <<existencias << " Seg:" << segundosFabricacion << " Ubi:"<<ubicacion <<endl;
     }
 
     int getExistencias(){
@@ -41,8 +45,8 @@ struct articulo{
 
 struct listaDoble{
     //Pointer section
-    articulo* primerNodo;
-    articulo* ultimoNodo;
+    Articulo* primerNodo;
+    Articulo* ultimoNodo;
 
     //Builder
 
@@ -52,12 +56,12 @@ struct listaDoble{
 
     //inserta un articulo al final de la lista doble
     //aka mete varas en la bodega
-    void insertar(articulo* nuevo){
+    void insertar(Articulo* nuevo){
         if(primerNodo == NULL){
             primerNodo = ultimoNodo = nuevo;
         }
         else{
-            articulo* actual = primerNodo;
+            Articulo* actual = primerNodo;
             while(actual->siguiente!=NULL){
                 actual = actual->siguiente;
             }
@@ -69,39 +73,58 @@ struct listaDoble{
     //Metodo para ver si ya el articulo existe en la lista doble
     /*Esto sirve para ver si esta repetido a la hora de cargar en memoria
 o para ver si existe cuando se procesa un pedido*/
-    bool exists(string _codigo){
-        articulo* actual = primerNodo;
-        while(actual->siguiente!=NULL){
-            if(actual->codigo == _codigo){
-                return true;
+    /*bool exists(string _codigo){
+        if(primerNodo != NULL){
+            Articulo* actual = primerNodo;
+            while(actual->siguiente!=NULL){
+                cout << "SE IMPRIME LO QUE HAYA ";
+                actual->imprimir();
+                if(actual->codigo == _codigo){
+                    return true;
+                }
+                actual = actual->siguiente;
+                }
             }
-            actual = actual->siguiente;
-        }
+        else{return true;}
         return false;
+    }*/
+
+    bool exists(string _codigo){
+        if(primerNodo != NULL){
+
+        }
     }
+
+
 
     //Metodo para ver si hay suficientes existencias de un producto en bodega
     bool suficiente(string _codigo, int cantidad){
-        articulo* actual = primerNodo;
-        while(actual->siguiente!=NULL){
-            if(actual->codigo == _codigo){ //se que se puede un and pero tengo pereza
-                if (actual->getExistencias() >= cantidad){
-                    return true;
+        if(primerNodo != NULL){
+            Articulo* actual = primerNodo;
+            while(actual->siguiente!=NULL){
+                if(actual->codigo == _codigo){ //se que se puede un and pero tengo pereza
+                    if (actual->getExistencias() >= cantidad){
+                        return true;
+                    }
                 }
+                actual = actual->siguiente;
             }
-            actual = actual->siguiente;
         }
+        else{return true;}
         return false;
     }
 
     bool ubicacionOcupada(string _ubicacion){
-        articulo* actual = primerNodo;
-        while(actual->siguiente!=NULL){
-            if(actual->getUbicacion() == _ubicacion){
-                return true;
+        if(primerNodo != NULL){
+            Articulo* actual = primerNodo;
+            while(actual->siguiente!=NULL){
+                if(actual->getUbicacion() == _ubicacion){
+                    return true;
+                }
+                actual = actual->siguiente;
             }
-            actual = actual->siguiente;
         }
+        else{return true;}
         return false;
     }
 
@@ -112,6 +135,7 @@ o para ver si existe cuando se procesa un pedido*/
             return;
         }
         string linea;
+        int cantidadAlm, tiempoFab;
         while (getline(archivo, linea)) {
             // Separar la línea en campos usando el tabulador como separador
             size_t pos1 = 0;
@@ -131,10 +155,43 @@ o para ver si existe cuando se procesa un pedido*/
 
             //Tratar de convertir a enteros
             if(isInt(cantidad)){
-                int cantidadAlm = stoi(cantidad);
+                cantidadAlm = stoi(cantidad);
             }else{
                 //Aqui tira el error y croma todo
             }
+
+            if(isInt(tiempo)){
+                tiempoFab = stoi(tiempo);
+            } else{
+                //Aqui tira error y croma
+            }
+
+            //cantidad menor que 0
+            if(cantidadAlm < 0){
+                //Error de cantidad menor a 0
+            }
+
+            //Tiempo de ejecucion menor a 0
+            if(tiempoFab <= 0){
+                //Error de tiempo de fabricacion menor igual a 0
+            }
+
+            //Verificar que sea tipo A,B o C
+            if(categoria != "A" || categoria != "B" || categoria != "C"){
+                //error de tipo
+            } //else no hace nada
+
+            if(exists(codigo)){
+               //error de ya existente
+            }
+
+            //if(ubicacionOcupada(ubicacion)){
+                //error de ubicacion ya ocupada
+            //}
+
+            //Ya paso todo y esta bien, se supone
+            Articulo* articulo = new Articulo(codigo, cantidadAlm, tiempoFab, categoria, ubicacion);
+            insertar(articulo);
         }
     }
 };
--- Proyecto1_ED/main.cpp
+++ Proyecto1_ED/main.cpp
@@ -1,7 +1,7 @@
 #include "includes.h"
 #include "CargarClientesMemoria.h"
-#include "SubirAMemoriaArticulos.h"
 #include "HiloPedidos.h"
+#include "articulos.h"
 #include "BalanceadorDecisiones.h"
 #include "ColaFacturacion.h"
 
@@ -12,44 +12,21 @@ int main(int argc, char *argv[])
 
     //Iniciar el hilo de lectura de pedidos
     FileRead hiloPedidos;
-    hiloPedidos.start(); //A este hilo tengo que pasarle ambas listas para luego poder verfiicar
+    //hiloPedidos.start();
 
-    //Cargar CLientes Memoria.h
+
     listaSimple clientes;
-    clientes.LeerClientes();
-    clientes.imprimir();
+    //clientes.LeerClientes();
+
+    listaDoble articulos;
+    articulos.cargar();
 
 
-    //list<Cliente> listaClientes;
-    //LeerClientes(listaClientes);
-
-
-//    //Se muestra la ventana con la interfaz gráfica
     QApplication a(argc, argv);
    Proyecto_ED w;
     w.show();
 
-//    //Iniciar el hilo de lectura de pedidos
-//    FileRead hiloPedidos;
-//    hiloPedidos.start();
 
-//    //Cargar CLientes Memoria.h
-//    list<Cliente> listaClientes;
-//    LeerClientes(listaClientes);
-//    cout<<"hola"<<endl;
-
-
-    // Se llama al encargado de facturar la cola de alistados
-
-
-//    colaFacturacion cola;
-//    cola.generarFacturas();
-//    return 0;
-
-
-    //Llamado del BalanceadorDecisiones
-
-    //BalanceadorDecisiones();
 
     return a.exec();
 
